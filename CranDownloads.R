be9fc0a544cce6ee82dc3e778d8674a24129e7b3
PhilippPro <philipp_probst@gmx.de>
2017-03-13 15:27:40 +0100

    Analyse the monthly cran_downloads of some packages

---
+++ CranDownloads.R
@@ -0,0 +1,30 @@
+library(cranlogs)
+library(data.table)
+
+packages = c("mlr", "caret", "radiant", "tscount", "ggplot2", "data.table", "rattle", "OOBCurve")
+dates = seq(as.Date("2016/7/1"), as.Date("2017/3/13"), by = "day")
+
+downloads_all = matrix(NA, length(dates)-30, length(packages))
+
+for(i in 31:length(dates)) { # moving window of 30 days
+  print(i)
+  downloads = cran_downloads(packages = packages, from = dates[i-30], to = dates[i])
+  downloads = data.table(downloads)
+  downloads = downloads[, sum(count), by = "package"]
+  downloads_all[as.numeric(i-30),] = downloads$V1
+}
+
+colnames(downloads_all) = packages
+
+pdf("cran_downloads.pdf")
+par(mfrow = c(1,1))
+plot(dates[31:length(dates)], downloads_all[,1], type = "l", col = "blue", ylab = "Number of downloads in the last month", xlab = "data") #,ylim = range(downloads_all))
+lines(dates[31:length(dates)], downloads_all[,2]/34, col = "red")
+legend("topleft", c("mlr", "caret (divided by 34)"), col = c("blue", "red"), lty = 1)
+abline(v = as.Date("2016-08-03"))
+abline(v = as.Date("2016-08-08"))
+abline(v = as.Date("2016-08-11"))
+abline(v = as.Date("2016-08-16"))
+abline(v = as.Date("2016-10-19"))
+abline(v = as.Date("2017-02-07")) # CRAN release of version 2.10
+dev.off()
