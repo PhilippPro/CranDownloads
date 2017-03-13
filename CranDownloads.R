9116b4ad89590c43f94ef549574cce42cb376765
PhilippPro <philipp_probst@gmx.de>
2017-03-13 15:33:10 +0100

    delete the weekend peaks

--- CranDownloads.R
+++ CranDownloads.R
@@ -4,22 +4,22 @@ library(data.table)
 packages = c("mlr", "caret", "radiant", "tscount", "ggplot2", "data.table", "rattle", "OOBCurve")
 dates = seq(as.Date("2016/7/1"), as.Date("2017/3/13"), by = "day")
 
-downloads_all = matrix(NA, length(dates)-30, length(packages))
+downloads_all = matrix(NA, length(dates)-27, length(packages))
 
-for(i in 31:length(dates)) { # moving window of 30 days
+for(i in 28:length(dates)) { # moving window of 28 days (to delete the weekend peaks)
   print(i)
-  downloads = cran_downloads(packages = packages, from = dates[i-30], to = dates[i])
+  downloads = cran_downloads(packages = packages, from = dates[i-27], to = dates[i])
   downloads = data.table(downloads)
   downloads = downloads[, sum(count), by = "package"]
-  downloads_all[as.numeric(i-30),] = downloads$V1
+  downloads_all[as.numeric(i-27),] = downloads$V1
 }
 
 colnames(downloads_all) = packages
 
 pdf("cran_downloads.pdf")
 par(mfrow = c(1,1))
-plot(dates[31:length(dates)], downloads_all[,1], type = "l", col = "blue", ylab = "Number of downloads in the last month", xlab = "data") #,ylim = range(downloads_all))
-lines(dates[31:length(dates)], downloads_all[,2]/34, col = "red")
+plot(dates[28:length(dates)], downloads_all[,1], type = "l", col = "blue", ylab = "Number of downloads in the last month", xlab = "data") #,ylim = range(downloads_all))
+lines(dates[28:length(dates)], downloads_all[,2]/34, col = "red")
 legend("topleft", c("mlr", "caret (divided by 34)"), col = c("blue", "red"), lty = 1)
 abline(v = as.Date("2016-08-03"))
 abline(v = as.Date("2016-08-08"))
