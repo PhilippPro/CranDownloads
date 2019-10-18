6754e27b0dd303d7605360c7b99fe0d9f3638979
Philipp Probst <philipp_probst@gmx.de>
2019-10-18 16:12:57 +0200

    update

--- CranDownloads.R
+++ CranDownloads.R
@@ -3,7 +3,7 @@ library(data.table)
 
 packages = c("mlr", "caret", "radiant", "tscount", "ggplot2", "data.table", "rattle", "OOBCurve", "quantregRanger", "mlrMBO", "rBayesianOptimization", "tuneRanger", "varImp", "keras", 
   "randomForest", "xgboost", "randomForestSRC", "ranger", "Rborist", "e1071", "kernlab", "liquidSVM", "kknn", "RWeka")
-dates = seq(as.Date("2016/7/1"), as.Date("2019/7/2"), by = "day")
+dates = seq(as.Date("2016/7/1"), as.Date("2019/10/15"), by = "day")
 
 downloads_all = matrix(NA, length(dates)-27, length(packages))
 
@@ -39,6 +39,7 @@ lines(dates[28:length(dates)], downloads_all[,"quantregRanger"], col = "red")
 lines(dates[28:length(dates)], downloads_all[,"tscount"], col = "violet")
 lines(dates[28:length(dates)], downloads_all[,"tuneRanger"], col = "green")
 lines(dates[28:length(dates)], downloads_all[,"varImp"], col = "black")
+legend("topleft", c("OOBCurve", "quantregRanger", "tscount", "tuneRanger", "varImp"),lty =1, col = c("blue", "red", "violet", "green", "black"))
 #lines(dates[28:length(dates)], downloads_all[,"liquidSVM"], col = "orange")
 
 plot(dates[28:length(dates)], downloads_all[,"mlrMBO"], type = "l", col = "blue", ylab = "Number of downloads in the last month", xlab = "data") #,ylim = range(downloads_all))
@@ -53,6 +54,8 @@ lines(dates[28:length(dates)], downloads_all[,"ranger"], col = "red")
 lines(dates[28:length(dates)], downloads_all[,"randomForestSRC"], col = "orange")
 lines(dates[28:length(dates)], downloads_all[,"Rborist"], col = "violet")
 lines(dates[28:length(dates)], downloads_all[,"xgboost"], col = "blue")
+legend("topleft", c("randomForest", "ranger", "randomForestSRC", "Rborist", "xgboost"),lty =1, col = c("black", "red", "orange", "violet", "blue"))
+
 
 plot(dates[28:length(dates)], downloads_all[,"e1071"], type = "l", col = "black", ylab = "Number of downloads in the last month", xlab = "data", ylim = c(0, max(downloads_all[,"e1071"]))) #,ylim = range(downloads_all))
 lines(dates[28:length(dates)], downloads_all[,"kernlab"], col = "red")
